71aecd5feeb089c4cceec2ed2ee36fbcd91d2d47
nasroon <nasroones@gmail.com>
2019-09-21 01:33:40 +0700

    3rd commit

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
     <div>
-    <WordCard value="world"/>
+      <WordCard value="world"/>
     </div>
     );
    }
--- card-game/src/WordCard.js
+++ card-game/src/WordCard.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard';
+import _ from 'lodash';
 
 
-export default class WordCard extends
-Component {
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: [],
+        completed: false
+    }
+}
+
+export default class WordCard extends Component {
+    
     activationHandler = c => { console.log(`${c} has been activated.`) }
     render() {
+        var str = this.props.value
+        var ranStr = str[Math.floor(Math.random()*str.length)]
+        //var reStr = x.replace(/o/g,"-")
         return (
         <div>
-        { Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler}/>) }
+        { Array.from(ranStr).map((c, i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler}/>) }
         </div>
         );
     }
 }
+
+

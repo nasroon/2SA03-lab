2720066ff009132c7b9c9caa2419776ae2afa583
nasroon <nasroones@gmail.com>
2019-09-21 02:17:51 +0700

    5th commit

--- card-game/src/WordCard.js
+++ card-game/src/WordCard.js
@@ -19,23 +19,15 @@ export default class WordCard extends Component {
     
     activationHandler = c => { console.log(`${c} has been activated.`) }
     render() {
-        var str = Array.from(this.props.value),
-        ranStr = [],
-        i = str.length,
-        j = 0;
 
-        while (i--) {
-            j = Math.floor(Math.random() * (i+1));
-            ranStr.push(str[j]);
-            str.splice(j,1);
-        }
-
+        
 
         return (
         <div>
-        { Array.from(ranStr).map((c, i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler}/>) }
+        { prepareStateFromWord(this.props.value).chars.map((c, i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler}/>) }
         </div>
         );
+        
     }
 }
 

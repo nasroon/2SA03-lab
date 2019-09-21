172fff1d3fb932148d44827151a34f5887a0358d
nasroon <nasroones@gmail.com>
2019-09-22 00:36:48 +0700

    12th commit

--- card-game/src/WordCard.js
+++ card-game/src/WordCard.js
@@ -11,15 +11,18 @@ const prepareStateFromWord = (given_word) => {
         chars,
         attempt: 1,
         guess: [],
+        round : 1,
         completed: false
     }
 }
 
-    
+let winrate = 0
 
 export default class WordCard extends Component {
+    
     constructor(props){
         super(props)
+        
         this.state = prepareStateFromWord(this.props.value)
     }
     
@@ -34,17 +37,22 @@ export default class WordCard extends Component {
             }
             else{
                 console.log("Wrong !! Let's play agian")
-                this.setState({guess: [], attempt: this.state.attempt + 1})
+                this.setState({guess: [], attempt: this.state.attempt + 1 , round: this.state.round +1})
             }
      }
 }
 
 regame = () => {
     if(this.state.completed == true){
-        this.setState({guess:[], attempt: 1 })
+        this.setState({guess:[], attempt: this.state.attempt + 1 })
+        winrate = winrate+1
+        document.getElementById("wr").innerHTML = "Win : " + winrate
+        this.setState({completed : false})
     }
+    //else{
+    //    this.setState({guess:[], attempt: 1 })
+    //}
 }
- 
     render() {
         return (
         <div >
@@ -52,10 +60,13 @@ regame = () => {
         { Array.from(this.state.chars).map((c, i) => <CharacterCard value={c} key={i} 
             attempt = {this.state.attempt}
             activationHandler={this.activationHandler} />) }
-            <br></br><br></br><br></br><h3>{this.state.completed? "WIN":""}</h3>
+            <br></br><h2 id="wr">Win : 0</h2>
+            <br></br><br></br><h3>{this.state.completed? "Win":""}</h3>
             <h2 className="tooltip">HINT
-            <span class="tooltiptext">Name of this lab</span>
+            <span className="tooltiptext">Name of this lab</span>
             </h2>
+            <br></br>
+            <button className="button" onClick={this.regame}>Play agian</button>
         </div>
         );
         
